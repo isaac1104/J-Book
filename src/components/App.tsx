095c67bbd8f13f755f139241af7aa6f351530a3b
Isaac Kwon <isaac-kwon@hotmail.com>
2021-02-16 11:42:01 -0600

    textarea wip

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setInput(e.target.value);
+  };
+  const handleClick = () => {
+    console.log(input);
+  };
+
   return (
     <div>
-      <h1>App</h1>
+      <textarea value={input} onChange={handleChange}></textarea>
+      <div>
+        <button onClick={handleClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
     </div>
   );
 };
