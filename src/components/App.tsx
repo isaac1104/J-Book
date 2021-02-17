cabe55a584694f379661689bc702c29af39e4632
Isaac Kwon <isaac-kwon@hotmail.com>
2021-02-17 09:39:10 -0600

    esbuild wip

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,40 @@
-import React, { useState } from 'react';
+import * as esbuild from 'esbuild-wasm';
+import React, { useState, useEffect, useRef } from 'react';
+import { unpkgPathPlugin } from '../plugins/unpkg-path-plugin';
+import { fetchPlugin } from '../plugins/fetch-plugin';
 
 const App = () => {
+  const ref = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.46/esbuild.wasm',
+    });
+  };
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setInput(e.target.value);
   };
-  const handleClick = () => {
-    console.log(input);
+  const handleClick = async () => {
+    if (!ref.current) return;
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
+    });
+    setCode(result.outputFiles[0].text);
   };
+
+  useEffect(() => {
+    startService();
+  }, []);
 
   return (
     <div>
