d97b95417069281afc293992b90e7adef7964242
Isaac Kwon <isaac-kwon@hotmail.com>
2021-02-17 16:40:35 -0600

    show bundled code inside iframe

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,8 +5,8 @@ import { fetchPlugin } from '../plugins/fetch-plugin';
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -19,6 +19,9 @@ const App = () => {
   };
   const handleClick = async () => {
     if (!ref.current) return;
+
+    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -29,12 +32,32 @@ const App = () => {
         global: 'window',
       },
     });
-    setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
 
   useEffect(() => {
     startService();
   }, []);
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id='root'></div>
+        <script>
+          window.addEventListener('message', e => {
+            try {
+              eval(e.data);
+            } catch (err) {
+              const root = document.getElementById('root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+              console.error(err);
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
 
   return (
     <div>
@@ -42,7 +65,12 @@ const App = () => {
       <div>
         <button onClick={handleClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe
+        title='preview'
+        ref={iframe}
+        sandbox='allow-scripts'
+        srcDoc={html}
+      />
     </div>
   );
 };
