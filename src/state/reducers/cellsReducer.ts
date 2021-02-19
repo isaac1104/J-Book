f9cf2263054f30e32aef98ca68e6fbc913ecbf6b
Isaac Kwon <isaac-kwon@hotmail.com>
2021-02-19 16:38:31 -0600

    redux setup & immer wip

---
+++ src/state/reducers/cellsReducer.ts
@@ -0,0 +1,41 @@
+import produce from 'immer';
+import { ActionType } from '../action-types';
+import { Action } from '../actions';
+import { Cell } from '../cell';
+
+interface CellsState {
+  loading: boolean;
+  error: string | null;
+  order: string[];
+  data: {
+    [key: string]: Cell;
+  };
+}
+
+const INITIAL_STATE: CellsState = {
+  loading: false,
+  error: null,
+  order: [],
+  data: {},
+};
+
+const cellsReducer = produce(
+  (state: CellsState = INITIAL_STATE, action: Action) => {
+    switch (action.type) {
+      case ActionType.UPDATE_CELL:
+        const { id, content } = action.payload;
+        state.data[id].content = content;
+        return;
+      case ActionType.DELETE_CELL:
+        return state;
+      case ActionType.MOVE_CELL:
+        return state;
+      case ActionType.INSERT_CELL_BEFORE:
+        return state;
+      default:
+        return state;
+    }
+  }
+);
+
+export default cellsReducer;
