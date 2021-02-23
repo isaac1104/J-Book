9c8541fa95b135d8807b7cd8e471dcee797a4848
Isaac Kwon <isaac-kwon@hotmail.com>
2021-02-22 21:13:52 -0600

    add margin to cell list

--- src/components/CellList/CellList.tsx
+++ src/components/CellList/CellList.tsx
@@ -1,3 +1,4 @@
+import './CellList.css';
 import React from 'react';
 import { useTypedSelector } from '../../hooks/useTypedSelector';
 import AddCell from '../AddCell';
@@ -9,7 +10,7 @@ const CellList: React.FC = () => {
   );
 
   return (
-    <div>
+    <div className='cell-list'>
       <AddCell forceVisible={cells.length === 0} prevCellId={null} />
       {cells.map(cell => (
         <React.Fragment key={cell.id}>
