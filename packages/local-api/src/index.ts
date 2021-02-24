68040a568f409ce22fa72f5e31a21d65f584b1b1
Isaac Kwon <isaac-kwon@hotmail.com>
2021-02-24 16:38:50 -0600

    production mode logic

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,16 +1,28 @@
 import express from 'express';
+import path from 'path';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
-export const serve = (port: number, filename: string, dir: string) => {
+export const serve = (
+  port: number,
+  filename: string,
+  dir: string,
+  useProxy: boolean
+) => {
   const app = express();
 
-  app.use(
-    createProxyMiddleware({
-      target: 'http://localhost:3000',
-      ws: true,
-      logLevel: 'silent',
-    })
-  );
+  const packagePath = require.resolve('local-client/build/index.html');
+
+  if (useProxy) {
+    app.use(
+      createProxyMiddleware({
+        target: 'http://localhost:3000',
+        ws: true,
+        logLevel: 'silent',
+      })
+    );
+  } else {
+    app.use(express.static(path.dirname(packagePath)));
+  }
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on('error', reject);
